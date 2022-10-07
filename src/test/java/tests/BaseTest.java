84b419005b9a55fe182cf61031b60a25d3a09a86
saurav <sauravbhr712@gmail.com>
2022-10-07 08:20:24 -0600

    updated code to work on chrome version 106.0.5249.61, and also updated the take full screenshot method

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,36 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import pages.HomePage;
+import utils.logs.Log;
+
+public class BaseTest {
+    public WebDriver driver;
+    public HomePage  homePage;
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    @BeforeClass
+    public void classSetup() {
+        Log.info("Tests starting!");
+        System.setProperty("webdriver.chrome.driver","Drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @BeforeMethod
+    public void Setup() {
+        homePage = new HomePage(driver);
+    }
+
+    @AfterClass
+    public void teardown() {
+        Log.info("Tests ended");
+        driver.quit();
+    }
+}
---
+++ src/test/java/tests/LoginTests.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+
+import static utils.extentreports.ExtentTestManager.startTest;
+
+public class LoginTests extends tests.BaseTest {
+    @Test(priority = 0, description = "valid Scenario with username ,password  and company.")
+    public void sendingDetailsToPage(Method method) {
+        //ExtentReports Description
+        startTest(method.getName(), "valid Scenario with username ,password  and company.");
+
+        homePage
+            .goToSelectorsHub()
+            .goToPracticePage()
+            .loginToSH("Saurav@gmail.com","tomtom@123","Dell");
+
+    }
+
+}
