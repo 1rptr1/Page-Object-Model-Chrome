d43570e0b18d24b837a446f380af1d5da5909c1e
saurav <sauravbhr712@gmail.com>
2024-09-14 12:28:37 +0530

    Added remote driver execution - added Docker files - added commands used
    Signed-off-by: saura <sauravbhr12@gmail.com>

--- src/main/java/utility/DriverFactory.java
+++ src/main/java/utility/DriverFactory.java
@@ -1,22 +1,30 @@
 package utility;
 
+import lombok.SneakyThrows;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
+
+import java.net.URL;
 
 public class DriverFactory {
 
     private DriverFactory(){
         throw  new IllegalStateException("Driver Factory Class");
     }
-    private static ThreadLocal<WebDriver> threadLocalDriver = new ThreadLocal<>();
+    private static final ThreadLocal<WebDriver> threadLocalDriver = new ThreadLocal<>();
 
     // Method to create and get the driver instance for the current thread
+    @SneakyThrows
     public static WebDriver getDriver() {
         if (threadLocalDriver.get() == null) {
             Properties propertiesReader = Properties.getInstance();
             String browserName = propertiesReader.getProperty("browser");
+            String remoteURL = propertiesReader.getProperty("remoteUrl");
 
             WebDriver driver;
             switch (browserName.toUpperCase()) {
@@ -28,6 +36,12 @@ public class DriverFactory {
                     break;
                 case "SAFARI":
                     driver = new SafariDriver();
+                    break;
+                case "REMOTE":
+                    ChromeOptions options = new ChromeOptions();
+                    DesiredCapabilities capabilities = new DesiredCapabilities();
+                    capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+                    driver = new RemoteWebDriver(new URL(remoteURL), capabilities);
                     break;
                 default:
                     throw new IllegalArgumentException("Invalid browser type: " + browserName);
